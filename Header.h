f9249b48ddfcf1e618400b4902511739bb848794
Левченко Владислав <90248828+mered1t@users.noreply.github.com>
2022-02-24 01:36:47 +0300

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void inputFile(string);
+void printFile(string);
+string spaceEdit(string);
+void changeFile(string, string);
---
+++ main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include "Header.h"
+
+int main()
+{
+    string intoFile = "inFile.txt";
+
+    inputFile(intoFile);
+ 
+    cout << endl << "Text: ";
+    printFile(intoFile);
+
+    string endFile = "finalFile.txt";
+    cout << "New text:";
+
+    changeFile(intoFile, endFile);
+    printFile(endFile);
+
+    cin.ignore(0, '\n');
+    cin.get();
+}
